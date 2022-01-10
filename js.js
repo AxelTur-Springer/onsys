0eddf0a15d740834250207909c390507d0c0ac2a
AxelTur-Springer <axel.turspringer@gmail.com>
2022-01-10 20:30:49 -0300

    subiendo

---
+++ js.js
@@ -0,0 +1,39 @@
+
+function disapear(){
+    document.getElementById("helloscreen").style.display = "none"
+    document.getElementById("grid-page").style.display = "block"
+}
+document.getElementById("helloscreen").style.display = "none"
+document.getElementById("grid-page").style.display = "block"
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
