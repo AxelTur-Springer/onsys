8dd7eaca1a786ab1e34b9393979ee27b484e7dc8
AxelTur-Springer <axel.turspringer@gmail.com>
2022-01-11 20:05:37 -0300

    avances

--- js.js
+++ js.js
@@ -3,8 +3,7 @@ function disapear(){
     document.getElementById("helloscreen").style.display = "none"
     document.getElementById("grid-page").style.display = "block"
 }
-document.getElementById("helloscreen").style.display = "none"
-document.getElementById("grid-page").style.display = "block"
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
